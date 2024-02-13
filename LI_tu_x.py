5c54f95f6088a693835d55aef96b267124354905
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-02-13 10:36:20 +0530

    Add files via upload

---
+++ LI_tu_x.py
@@ -0,0 +1,12 @@
+# list use to store the value
+list1 = [1,2,3,'A','B',7,8,[10,11]]
+print(list1)
+
+# show the list bracket
+x=list()
+print(x)
+
+# tuple to list
+tuple1 = (1,2,3,4)
+x=list(tuple1)
+print(x)
